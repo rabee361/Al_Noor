b653af4f9e4949e6500ad3db752d560fb38486a8
rabee361 <rha60540@gmail.com>
2024-04-14 18:35:27 +0300

    changes

--- Al_Noor/urls.py
+++ Al_Noor/urls.py
@@ -1,9 +1,11 @@
-
 from django.contrib import admin
 from django.urls import path , include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('al-noor/admin/', admin.site.urls),
     path('al-noor/api/' , include('base.urls')),
     path('al-noor/myadmin/' , include('admin_panel.urls'))
-]
+    
+]+ static(settings.MEDIA_URL , document_root=settings.MEDIA_ROOT)
