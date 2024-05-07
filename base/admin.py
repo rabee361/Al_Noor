59454320741b66c7ceb5d062542cf495929d4750
rabee361 <rha60540@gmail.com>
2024-05-07 21:36:03 +0300

    changes

--- base/admin.py
+++ base/admin.py
@@ -13,7 +13,7 @@ admin.site.site_header = "Dashboard"
 admin.site.index_title = "Admin Panel"
 
 
-admin.site.unregister(FCMDevice)
+# admin.site.unregister(FCMDevice)
 admin.site.unregister(BlacklistedToken)
 admin.site.unregister(OutstandingToken)
 admin.site.unregister(Group)
@@ -22,9 +22,9 @@ class HiddenModelAdmin(admin.ModelAdmin):
     def get_model_perms(self, request):
         return {}
     
-@admin.register(FCMDevice)
-class FCMDeviceAdmin(HiddenModelAdmin):
-    pass
+# @admin.register(FCMDevice)
+# class FCMDeviceAdmin(HiddenModelAdmin):
+#     pass
 
 @admin.register(OutstandingToken)
 class OutstandingTokenAdmin(HiddenModelAdmin):
