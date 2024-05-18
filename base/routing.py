185b4a2eec80eb2c1a754563e9d26969471e7884
rabee361 <rha60540@gmail.com>
2024-05-18 09:20:02 +0300

    changes

--- base/routing.py
+++ base/routing.py
@@ -2,6 +2,7 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r"al-noor/ws/employee-chat/(?P<chat_id>\d+)/(?P<user_id>\d+)$", consumers.CreateEmployeeMessage.as_asgi()),
+    re_path(r"al-noor/ws/manager-chat/(?P<chat_id>\d+)/(?P<user_id>\d+)$", consumers.CreateEmployeeMessage.as_asgi()),
+    # re_path(r"al-noor/ws/employee-chat/(?P<chat_id>\d+)/(?P<user_id>\d+)$", consumers.CreateEmployeeMessage.as_asgi()),
     re_path(r"al-noor/ws/guide-chat/(?P<chat_id>\d+)/(?P<user_id>\d+)$", consumers.CreateGuideMessage.as_asgi()),
 ]
