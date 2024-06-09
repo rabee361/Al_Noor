d3937e01bc5b9ccd0a2639d18561c6d2aa7ce331
rabee361 <rha60540@gmail.com>
2024-06-09 17:40:19 +0300

    changes

--- Al_Noor/urls.py
+++ Al_Noor/urls.py
@@ -4,9 +4,9 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('myadmin/', admin.site.urls),
     path('api/' , include('base.urls')),
-    path('myadmin/' , include('admin_panel.urls')),
+    path('admin/' , include('admin_panel.urls')),
     path('myapi/' , include('admin_panel.api.urls')),
 
 ]+ static(settings.MEDIA_URL , document_root=settings.MEDIA_ROOT)
