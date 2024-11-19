c44eb178f63be8f9b74e1bdee4db40957b294f9b
rabee361 <rha60540@gmail.com>
2024-11-19 21:42:50 +0300

    fix

--- base/utils/notifications.py
+++ base/utils/notifications.py
@@ -21,25 +21,25 @@ def send_task_notification(employee,title,content):
 
 
 
-def send_event_notification(title,content):
-    users = CustomUser.objects.filter(Q(user_type='حاج') & Q(get_notifications=True))
-    for user in users:
-        devices = FCMDevice.objects.filter(user=user.id)
-        devices.send_message(
-                message =Message(
-                    notification=Notification(
-                        title=title,
-                        body=content
-                    ),
-                ),
-            )
-        UserNotification.objects.create(user=user,content=content,title=title)
+# def send_event_notification(title,content,sentBy):
+#     users = CustomUser.objects.filter(Q(user_type='حاج') & Q(get_notifications=True))
+#     for user in users:
+#         devices = FCMDevice.objects.filter(user=user.id)
+#         devices.send_message(
+#                 message =Message(
+#                     notification=Notification(
+#                         title=title,
+#                         body=content
+#                     ),
+#                 ),
+#             )
+#         UserNotification.objects.create(user=user,content=content,title=title)
     
 
 
 
-def send_pilgrims_notification(title,content,user):
-    guide = Guide.objects.get(user=user)
+def send_pilgrims_notification(title,content,sentBy):
+    guide = Guide.objects.get(user=sentBy)
     pilgrims = Pilgrim.objects.filter(guide=guide).values('user')
     users = CustomUser.objects.filter((Q(user_type='حاج') & Q(get_notifications=True) )& Q(id__in = pilgrims))
     for user in users:
@@ -53,12 +53,12 @@ def send_pilgrims_notification(title,content,user):
                 ),
             )
         UserNotification.objects.create(user=user,content=content,title=title)
-    BaseNotification.objects.create(title=title,content=content,info="لا يوجد",sentBy=user)
+    BaseNotification.objects.create(title=title,content=content,info="لا يوجد",sentBy=sentBy)
     
 
 
 
-def send_event_notification(title,content,user):
+def send_event_notification(title,content,sentBy):
     users = CustomUser.objects.filter(Q(user_type='حاج') & Q(get_notifications=True))
     for user in users:
         devices = FCMDevice.objects.filter(user=user.id)
@@ -71,7 +71,7 @@ def send_event_notification(title,content,user):
                 ),
             )
         UserNotification.objects.create(user=user,content=content,title=title)
-    BaseNotification.objects.create(title=title,content=content,info="لا يوجد",sentBy=user)
+    BaseNotification.objects.create(title=title,content=content,info="لا يوجد",sentBy=sentBy)
 
 
 
