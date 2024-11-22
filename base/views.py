9e14f5d2df7e33749b8615057d341861d9953f66
rabee361 <rha60540@gmail.com>
2024-11-22 22:08:30 +0300

    guide id fix

--- base/views.py
+++ base/views.py
@@ -60,7 +60,7 @@ class LoginUser(GenericAPIView):
                     if pilgrim.guide:
                         data['guide_image'] = request.build_absolute_uri(pilgrim.guide.user.image.url) or None
                         data['guide_name'] = pilgrim.guide.user.username
-                        data['guide_id'] = pilgrim.guide.id
+                        data['guide_id'] = pilgrim.guide.user.id
                     else:
                         data['guide_id'] = 0
                 except Pilgrim.DoesNotExist:
