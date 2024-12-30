d4d007e1544444b8a16f719e4c0718cf6107cd3a
rabee361 <rha60540@gmail.com>
2024-12-30 20:26:39 +0300

    url fix

--- Al_Noor/urls.py
+++ Al_Noor/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('api/' , include('base.urls')),
     path('admin/' , include('admin_panel.urls')),
     path('myapi/' , include('admin_panel.api.urls')),
-    path('welcome/' , LandinPageView.as_view() , name="welcome"),
+    path('' , LandinPageView.as_view() , name="welcome"),
     path('form/' , PilgrimFormView.as_view() , name="form"),
 
 ]+ static(settings.MEDIA_URL , document_root=settings.MEDIA_ROOT)
