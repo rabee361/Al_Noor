60cbb899f98a46d95edf4479bfa0247987a00f44
rabee361 <rha60540@gmail.com>
2025-02-07 07:33:46 +0300

    notification fix

--- base/utils/notifications.py
+++ base/utils/notifications.py
@@ -43,7 +43,7 @@ def send_pilgrims_notification(title,content,sentBy):
     pilgrims = Pilgrim.objects.filter(guide=guide).values('user')
     users = CustomUser.objects.filter((Q(user_type='حاج') & Q(get_notifications=True) )& Q(id__in = pilgrims))
     for user in users:
-        devices = FCMDevice.objects.filter(user=user.id).fisrt()
+        devices = FCMDevice.objects.filter(user=user.id)
         devices.send_message(
                 message =Message(
                     notification=Notification(
